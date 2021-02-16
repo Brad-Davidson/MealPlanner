ccaeaa7f0084a63cf3bc3b8c3f42455715caf1d4
Brad Davidson <65302404+Brad-Davidson@users.noreply.github.com>
2021-02-15 19:08:59 -0500

    Dependencies for testing + Entity/gson

--- app/src/main/java/com/main/mealplanner/dto/Recipe.kt
+++ app/src/main/java/com/main/mealplanner/dto/Recipe.kt
@@ -5,7 +5,7 @@ import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 @Entity(tableName="recipe")
-data class Plant(var Meal: String, var Country : String, var time : int, var Ingredients : String, var Instructions : String, @PrimaryKey @SerializedName("id") var RecipeId:Int = 0) {
+data class Recipe(var Meal: String, var Country : String, var time : Int, var Ingredients : String, var Instructions : String, @PrimaryKey @SerializedName("id") var RecipeId:Int = 0) {
     override fun toString(): String {
         return Meal
     }

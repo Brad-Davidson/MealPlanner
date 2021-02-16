ff5809c1aba4079901732f874b4505c40e4c2c1f
Brad Davidson <65302404+Brad-Davidson@users.noreply.github.com>
2021-02-16 16:44:46 -0500

    implemented MealPlan "Owner"

--- app/src/main/java/com/main/mealplanner/dto/MealPlan.kt
+++ app/src/main/java/com/main/mealplanner/dto/MealPlan.kt
@@ -3,9 +3,11 @@ package com.main.mealplanner.dto
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
+import java.util.*
 
 @Entity(tableName="mealplan")
-data class MealPlan(var CookSchedule: DateTime, var RecipeId : String, @PrimaryKey @SerializedName("id") var MealPlanId:Int = 0) {
+data class MealPlan(var CookSchedule: Date, var RecipeId : String, var OwnerID : String, @PrimaryKey @SerializedName("id") var MealPlanId:Int = 0) {
     override fun toString(): String {
-        return CookSchedule
+        return "Cook Recipe #" + RecipeId + " at " + CookSchedule.toString()
     }
+}

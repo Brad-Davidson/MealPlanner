9a03e99508972ccf5955a6d181e0d54112fa74da
Brad Davidson <65302404+Brad-Davidson@users.noreply.github.com>
2021-02-16 16:24:37 -0500

    made class names unique

--- app/src/main/java/com/main/mealplanner/dto/Ingredient.kt
+++ app/src/main/java/com/main/mealplanner/dto/Ingredient.kt
@@ -4,8 +4,8 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
-@Entity(tableName="recipe")
-data class Recipe(var NearStore: String, var Quantity : Int, var Name : String, @PrimaryKey @SerializedName("id") var IngredientId:Int = 0) {
+@Entity(tableName="ingredient")
+data class Ingredient(var NearStore: String, var Quantity : Int, var Name : String, @PrimaryKey @SerializedName("id") var IngredientId:Int = 0) {
     override fun toString(): String {
         return Name
     }
--- app/src/main/java/com/main/mealplanner/dto/MealPlan.kt
+++ app/src/main/java/com/main/mealplanner/dto/MealPlan.kt
@@ -4,8 +4,8 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
-@Entity(tableName="recipe")
-data class Recipe(var CookSchedule: DateTime, var RecipeId : String, @PrimaryKey @SerializedName("id") var MealPlanId:Int = 0) {
+@Entity(tableName="mealplan")
+data class MealPlan(var CookSchedule: DateTime, var RecipeId : String, @PrimaryKey @SerializedName("id") var MealPlanId:Int = 0) {
     override fun toString(): String {
         return CookSchedule
     }

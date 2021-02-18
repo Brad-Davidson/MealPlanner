16676f95f5fc8a6aead9027f0a26b3b275dc522c
bodensjr <70085015+bodensjr@users.noreply.github.com>
2021-02-18 15:31:12 -0500

    Create MealPlannerDatabase.kt

---
+++ app/src/main/java/com/main/mealplanner/dao/MealPlannerDatabase.kt
@@ -0,0 +1,9 @@
+package com.main.mealplanner.dao
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.main.mealplanner.dto.MealPlan
+
+@Database(entities=arrayOf(MealPlan::class), version = 1)
+abstract  class MealPlannerDatabase : RoomDatabase() {
+    abstract fun mealPlanDAO() : IMealPlanDAO

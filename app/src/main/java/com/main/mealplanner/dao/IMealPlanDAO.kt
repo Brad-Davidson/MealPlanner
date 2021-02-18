56b4a647265ba7bde1348462a1fc0890fc021ce8
bodensjr <70085015+bodensjr@users.noreply.github.com>
2021-02-18 15:27:31 -0500

    Create IMealPlanDAO.kt

---
+++ app/src/main/java/com/main/mealplanner/dao/IMealPlanDAO.kt
@@ -0,0 +1,21 @@
+package com.main.mealplanner.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.main.mealplanner.dto.MealPlan
+
+@Dao
+interface IMealPlanDAO {
+
+    @Query("SELECT * FROM MealPlan")
+    fun getAllMealPlans()  : LiveData<List<MealPlan>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(mealplans: ArrayList<MealPlan>)
+
+    @Delete
+    fun delete(mealplan: MealPlan)
+
+    @Insert
+    fun save(mealplan: MealPlan)
+}

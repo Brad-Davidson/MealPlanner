9542d5df3e6f9f7b0530fc1a2370770e564597bc
Brad Davidson <65302404+Brad-Davidson@users.noreply.github.com>
2021-02-27 17:18:34 -0500

    Reverted comments in MealPlanService

--- app/src/main/java/com/main/mealplanner/service/MealPlanService.kt
+++ app/src/main/java/com/main/mealplanner/service/MealPlanService.kt
@@ -20,7 +20,11 @@ MealPlanService(application: Application) {
     private val application = application
 
     internal suspend fun fetchMealPlans(MealPlanId: String) {
+        withContext(Dispatchers.IO) {
+            val service = RetrofitClientInstance.retrofitInstance?.create(IMealPlanDAO::class.java)
+            val mealplans = async { service?.getAllMealPlans() }
 
+            this.updateMealPlans(mealplans.await())
         }
     }
 
@@ -30,23 +34,25 @@ MealPlanService(application: Application) {
     private suspend fun updateMealPlans(mealplans: ArrayList<MealPlan>?) {
         var sizeOfMealPlans = mealplans?.size
         try {
-            //var MealPlanDAO = getMealPlanDAO()
-            //MealPlanDAO.insertAll(mealplans!!)
-        }catch (e: Exception) {
+            var MealPlanDAO = getMealPlanDAO()
+            MealPlanDAO.insertAll(mealplans!!)
+        } catch (e: Exception) {
             e.message?.let { Log.e(TAG, it) }
         }
 
     }
 
-    //internal fun getMealPlanDAO() : IMealPlanDAO {
-        //val db = Room.databaseBuilder(application, MealPlanDatabase::class.java, "mymealplans").build()
-        //val MealPlanDAO = db.MealPlanDAO()
-       //return MealPlanDAO
-    //}
+    internal fun getMealPlanDAO(): IMealPlanDAO {
+        val db = Room.databaseBuilder(application, MealPlanDatabase::class.java, "mymealplans").build()
+        val MealPlanDAO = db.MealPlanDAO()
+        return MealPlanDAO
+        }
 
-    internal fun save(mealplan:MealPlan) {
-        //getMealPlanDAO().save(mealplan)
+        internal fun save(mealplan: MealPlan) {
+            getMealPlanDAO().save(mealplan)
+        }
     }
+}
 
 
 

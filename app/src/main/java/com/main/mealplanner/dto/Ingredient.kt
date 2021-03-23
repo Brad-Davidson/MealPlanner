e4caccce4f38601ccdf2d6870ad0ccfcfc5dc27b
bodensjr <70085015+bodensjr@users.noreply.github.com>
2021-03-23 18:49:10 -0400

    Update Ingredient.kt

--- app/src/main/java/com/main/mealplanner/dto/Ingredient.kt
+++ app/src/main/java/com/main/mealplanner/dto/Ingredient.kt
@@ -5,7 +5,7 @@ import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 @Entity(tableName="ingredient")
-data class Ingredient(var NearStore: String, var Quantity : Int, var Name : String, var Amounts: ArrayList<String>, @PrimaryKey @SerializedName("id") var IngredientId:Int = 0) {
+data class Ingredient(var NearStore: String, var Quantity : Int, var Name : String, @PrimaryKey @SerializedName("id") var IngredientId:Int = 0) {
     override fun toString(): String {
         return Name
     }

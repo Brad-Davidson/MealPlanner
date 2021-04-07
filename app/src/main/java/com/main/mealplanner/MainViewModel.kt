ccc15f0eecb8d423d5e6eaef361c0d14724b29b2
bodensjr <70085015+bodensjr@users.noreply.github.com>
2021-04-07 18:13:22 -0400

    Added some of the functions needed for firebase

--- app/src/main/java/com/main/mealplanner/MainViewModel.kt
+++ app/src/main/java/com/main/mealplanner/MainViewModel.kt
@@ -4,6 +4,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
+import com.google.firebase.storage.FirebaseStorage
 import com.main.mealplanner.dto.MealPlan
 import com.main.mealplanner.dto.RecipeDetails
 import com.main.mealplanner.dto.RecipeHeader
@@ -15,16 +17,38 @@ class MainViewModel : ViewModel() {
     var recipes: MutableLiveData<ArrayList<RecipeHeader>> = MutableLiveData()
     var recipeService: RecipeService = RecipeService()
     lateinit var firestore : FirebaseFirestore
+    var storageReferenence = FirebaseStorage.getInstance().getReference()
+    var mealplans : MutableLiveData<ArrayList<MealPlan>> = MutableLiveData<ArrayList<MealPlan>>()
+    var mealplan = MealPlan()
     
     init {
+            firestore = FirebaseFirestore.getInstance()
+            firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
             listenToRecipes()
+            listenToMealPlans()
     }
     
     private fun listenToRecipes() {
         fetchAllRecipes()
     }
 
-
+    fun save(
+        mealplan: MealPlan,
+        user: FirebaseUser
+    ) {
+        val document =
+            if (mealplan.mealplanId != null && !mealplan.mealplanId.isEmpty()) {
+                // updating existing
+                firestore.collection("mealplans").document(mealplan.mealplanId)
+            } else {
+                // create new
+                firestore.collection("mealplans").document()
+            }
+        mealplan.mealplanId = document.id
+        val set = document.set(mealplan)
+            set.addOnSuccessListener {
+                Log.d("Firebase", "document saved")
+    }
 
     fun fetchAllRecipes(){
         recipes = recipeService.fetchRecipeHeaders()
